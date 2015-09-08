763a80e65ae5be627972f5bc93add9985217199d
Kd Johar <kdjohar91@gmail.com>
2015-09-08 15:18:49 +0530

    bug fix

--- folio/folio/settings.py
+++ folio/folio/settings.py
@@ -52,21 +52,6 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'folio.urls'
 
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
 
 WSGI_APPLICATION = 'folio.wsgi.application'
 
