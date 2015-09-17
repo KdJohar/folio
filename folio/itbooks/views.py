2bf61b190f8f23a2a0d38e527b1041e43e92cb3c
Kd Johar <kdjohar91@gmail.com>
2015-09-18 03:16:57 +0530

    tags added

--- folio/itbooks/views.py
+++ folio/itbooks/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render_to_response
-from .models import Book, SearchTag
+from .models import Book, SearchTag, Tag
 from django.shortcuts import RequestContext, get_object_or_404
 from django.core.paginator import Paginator
 # Create your views here.
@@ -8,7 +8,7 @@ def home(request):
     title = 'Download Free Books'
     books = Book.objects.all().order_by('?')[:12]
     recent_books = Book.objects.all().order_by('-date')[:16]
-
+    tags = Tag.objects.all()
     return render_to_response('index.html', locals(), context_instance=RequestContext(request))
 
 def itbook_detail(request, slug):
@@ -25,6 +25,7 @@ def search_result(request):
     recent_books = Book.objects.all().order_by('-date')[:16]
     search_query = request.GET.get('query', '')
     title = 'search '+search_query
+    tags = Tag.objects.all()
     if len(search_query) == 0:
         title = 'Error'
         error = 1
@@ -44,12 +45,8 @@ def search_result(request):
     page_num = request.GET.get('page', 1)
     page = paginator.page(page_num)
 
-    if not books:
-        books = None
-        return render_to_response('searchresults.html', locals(), context_instance=RequestContext(request))
-
-
     ctx = {
+        'tags':tags,
         'query': search_query,
         'title' : title,
         'page' : page,
@@ -57,3 +54,4 @@ def search_result(request):
         'meta_description' : meta_description
     }
     return render_to_response('searchresults.html', ctx, context_instance=RequestContext(request))
+
