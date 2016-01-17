0b1081f1c0461a626552ac1a2d919598334d895b
kdjohar <kdjohar91@gmail.com>
2016-01-17 20:48:41 +0530

    haystack search

--- folio/folio/settings.py
+++ folio/folio/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.sitemaps',
 
     'rest_framework',
-
+    'haystack',
     'itbooks',
 
 
@@ -87,6 +87,16 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100
 }
+
+HAYSTACK_CONNECTIONS = {
+    'default': {
+        'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
+        'URL': 'http://127.0.0.1:9200/',
+        'INDEX_NAME': 'haystack',
+    },
+}
+
+HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.RealtimeSignalProcessor'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
--- folio/itbooks/forms.py
+++ folio/itbooks/forms.py
@@ -1,7 +1,62 @@
 from django import forms
 from .models import SearchTag
-class SearchForm(forms.ModelForm):
-    search = forms.CharField(max_length=250)
-    class Meta:
-        model = SearchTag
-        fields = ['search']
+
+
+from haystack.forms import SearchForm, SearchQuerySet, EmptySearchQuerySet
+from django.utils.translation import ugettext_lazy as _
+
+class ItSearchForm(SearchForm):
+
+    def search(self):
+        # First, store the SearchQuerySet received from other processing.
+        sqs = super(ItSearchForm, self).search()
+
+        if not self.is_valid():
+            return self.no_query_found()
+
+        return sqs
+
+class SearchForm(SearchForm):
+    q = forms.CharField(required=False, label=_('Search'),
+                        widget=forms.TextInput(attrs={'type': 'search'}))
+
+    def __init__(self, *args, **kwargs):
+        self.searchqueryset = kwargs.pop('searchqueryset', None)
+        self.load_all = kwargs.pop('load_all', False)
+
+        if self.searchqueryset is None:
+            self.searchqueryset = SearchQuerySet()
+
+        super(SearchForm, self).__init__(*args, **kwargs)
+
+    def no_query_found(self):
+        """
+        Determines the behavior when no query was found.
+
+        By default, no results are returned (``EmptySearchQuerySet``).
+
+        Should you want to show all results, override this method in your
+        own ``SearchForm`` subclass and do ``return self.searchqueryset.all()``.
+        """
+        return EmptySearchQuerySet()
+
+    def search(self):
+        if not self.is_valid():
+            return self.no_query_found()
+
+        if not self.cleaned_data.get('q'):
+            return self.no_query_found()
+
+        sqs = self.searchqueryset.auto_query(self.cleaned_data['q'])
+
+        if self.load_all:
+            sqs = sqs.load_all()
+
+        return sqs
+
+    def get_suggestion(self):
+        if not self.is_valid():
+            return None
+
+        return self.searchqueryset.spelling_suggestion(self.cleaned_data['q'])
+
